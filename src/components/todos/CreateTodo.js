50e0cc57083b9793830ab280a6a526a9cd00609c
Lisa Lee <snickers495@gmail.com>
2018-02-09 15:47:19 +0000

    automatically backed up by learn

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -25,19 +25,18 @@ class CreateTodo extends Component {
   }
 
   render() {
-    return(
-      <div>
-        <form onSubmit={(event) => this.handleSubmit(event)}>
-          <p>
-            <label>add todo</label>
-            <input type="text" onChange={(event) => this.handleChange(event)} />
-          </p>
-          <input type="submit" />
-        </form>
-        {this.state.text}
-      </div>
-    );
-  }
-};
+  return(
+    <div>
+      <form onSubmit={(event) => this.handleSubmit(event)}>
+        <p>
+          <label>add todo</label>
+          <input type="text" value={this.state.text} onChange={(event) => this.handleChange(event)} />
+        </p>
+        <input type="submit" />
+      </form>
+      /* code removed here */
+    </div>
+  );
+}
 
 export default CreateTodo;
